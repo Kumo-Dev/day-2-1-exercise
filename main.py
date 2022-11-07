ac80a0238e78f9eab769ca71e29c73d2bf25960c
Kumo Dev <37561299+Kumo-Dev@users.noreply.github.com>
2022-11-07 19:44:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+num_string = str(two_digit_number)
+
+totalVal = int(num_string[0]) + int(num_string[1])
+
+print(totalVal)
+
